86907d0d0cbae13877c3e8efe42eefa9a8cf192a
G0d3r <ernest.godlewski@icloud.com>
2019-03-16 19:07:26 +0100

    Configured imageArray

---
+++ images+Names.swift
@@ -0,0 +1,19 @@
+//
+//  images+Names.swift
+//  accelerometron
+//
+//  Created by ErnestG on 16/03/2019.
+//  Copyright © 2019 EG. All rights reserved.
+//
+
+import UIKit
+
+var camera = UIImage(named: "1")!
+var city = UIImage(named: "2")!
+var animals = UIImage(named: "3")!
+var flowers = UIImage(named: "4")!
+var stand = UIImage(named: "5")!
+var urban = UIImage(named: "6")!
+
+var imageArray = [camera, city, animals, flowers, stand, urban]
+var nameArray = ["CAMERAS", "CITY", "ANIMALS", "FLOWERS", "STANDS", "URBAN" ]
